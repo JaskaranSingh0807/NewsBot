9c7f81fc8cd3f042ccf570998bc77ba6cc084e50
JaskaranSingh0807 <jaskaransunny73@gmail.com>
2024-06-26 11:46:14 +0530

    Add files via upload

---
+++ NewsBot.py
@@ -0,0 +1,46 @@
+import requests
+import streamlit as st
+import pyttsx3
+engine = pyttsx3.init()
+def speak_news(news_text):
+    engine.say(news_text)
+    engine.runAndWait()
+# Function to fetch news based on category
+def fetch_news(category, api_key):
+    if category == "Indian News":
+        url = f"https://newsapi.org/v2/top-headlines?country=in&apiKey={api_key}"
+    elif category == "BBC News":
+        url = f"https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey={api_key}"
+    elif category == "Hacker News":
+        url = f"https://newsapi.org/v2/top-headlines?sources=hacker-news&apiKey={api_key}"
+    else:
+        url = f"https://newsapi.org/v2/top-headlines?apiKey={api_key}&q={category.lower()}"
+    
+    response = requests.get(url)
+    data = response.json()
+    return data.get("articles", [])
+
+# Streamlit app
+def main():
+    st.title("Top News Bot Headlines")
+
+    st.sidebar.title("News Options")
+    api_key = "b7175abb766646199d8a2f07553a7a00"
+    category = st.sidebar.selectbox("Select News Category", ("Indian News", "BBC News", "Hacker News", "Sports", "Technology", "Science", "Health", "Entertainment"))
+    
+    if st.sidebar.button("Get News"):
+        news_articles = fetch_news(category, api_key)
+        
+        if news_articles:
+            for article in news_articles:
+                st.subheader(article['title'])
+                
+                st.write(article['content'])
+                
+                st.markdown(f"[Read more...]({article['url']})")
+                
+        else:
+            st.write("No news found for this category.")
+
+if __name__ == "__main__":
+    main()
